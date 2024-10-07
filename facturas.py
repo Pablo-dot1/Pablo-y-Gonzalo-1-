df19371a5386499ba2791dab5aaa11fce4597435
Gonzalo <gonzalolarriba@gmail.com>
2024-10-07 17:50:10 +0200

    Gonzalo modifica el código inicial  modificando la validación en la entrada del usuario aceptando valores únicamente mayores de 30 y guarda los cambios con control+S

--- facturas.py
+++ facturas.py
@@ -1,6 +1,6 @@
 # Solicitar los costos de los servicios básicos
 electricidad = float(input("Ingresa el costo de la electricidad: $"))
-if electricidad < 10:
+if electricidad < 20:
     print("El costo de la electricidad no puede ser negativo.")
     exit()
 
