ff34287ef6f83e5476a9cfbe266b66a3ad24b7da
Pablo-dot1 <pablo.huidobro.garcia@gmail.com>
2024-10-07 17:38:16 +0200

    Pablo en este caso ha creado un código inicial llamado facturas.py donde se solicita al usuario que ingrese los costos de tres servicios básicos del hogar: electricidad, agua y gas .En el caso de la electricidad se realiza una validación de la entrada del usuario y asegurarse que el valor no es negativo. Luego, suma estos valores y muestra el total de la factura formateado con dos decimales. Utiliza funciones simples como input() para recibir los datos y print() para mostrar el resultado

---
+++ facturas.py
@@ -0,0 +1,14 @@
+# Solicitar los costos de los servicios básicos
+electricidad = float(input("Ingresa el costo de la electricidad: $"))
+if electricidad < 0:
+    print("El costo de la electricidad no puede ser negativo.")
+    exit()
+
+agua = float(input("Ingresa el costo del agua: $"))   
+gas = float(input("Ingresa el costo del gas: $"))
+
+# Calcular el total de la factura
+total_factura = electricidad + agua + gas 
+
+# Mostrar el total de la factura
+print(f"\nEl total de la factura es: ${total_factura:.2f}")
